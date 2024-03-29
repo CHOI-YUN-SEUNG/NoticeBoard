65329ffb598d04605d7080eef5b88b079502b594
CHOI-YUN-SEUNG <ryans03@naver.com>
2024-03-29 10:42:23 +0900

    add class Board's printMenu() method

--- NoticeBoard/src/NoticeBoard/Board.java
+++ NoticeBoard/src/NoticeBoard/Board.java
@@ -5,11 +5,35 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class Board {
-	private Scanner scanner = new Scanner(System.in);
-	private UserManager userManager = new UserManager();
-	private PostManager postManager = new PostManager();
+	private Scanner scanner;
+	private UserManager userManager;
+	private PostManager postManager;
+
+	public Board() {
+		scanner = new Scanner(System.in);
+		userManager = new UserManager();
+		postManager = new PostManager();
+	}
 
 	public void run() {
-
+		while (true) {
+			printMenu();
+			System.out.print("선택>> ");
+		}
 	}
+
+	private void printMenu() {
+		System.out.println("\n===== 메뉴 =====");
+		System.out.println("1. 회원가입");
+		System.out.println("2. 로그인");
+		System.out.println("3. 로그아웃");
+		System.out.println("4. 회원탈퇴");
+		System.out.println("5. 게시판 둘러보기");
+		System.out.println("6. 게시글 작성");
+		System.out.println("7. 내 게시글 관리");
+		System.out.println("0. 종료");
+		System.out.println("===============");
+	}
+	
+	
 }
