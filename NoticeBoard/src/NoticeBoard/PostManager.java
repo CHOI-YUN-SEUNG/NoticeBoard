0242c5a8b4b554a4f5c298834717724b8271c69b
CHOI-YUN-SEUNG <ryans03@naver.com>
2024-03-29 10:30:10 +0900

    Create class PosterManager and add CRUD (temp)

--- NoticeBoard/src/NoticeBoard/PostManager.java
+++ NoticeBoard/src/NoticeBoard/PostManager.java
@@ -1,5 +1,35 @@
 package NoticeBoard;
 
-public class PostManager {
+import java.util.HashMap;
 
+public class PostManager {
+	private HashMap<String, UserPost> posts;
+	private int count;
+
+	public PostManager() {
+		posts = new HashMap<>();
+		count = 0;
+	}
+
+	public void addBoard(String title, String content, RegisteredUser author) {
+		UserPost post = new UserPost(title, content, author);
+		posts.put(title, post);
+		count++;
+	}
+
+	public UserPost getPost(String title) {
+		return posts.get(title);
+	}
+
+	public HashMap<String, UserPost> getPosts() {
+		return posts;
+	}
+
+	public void removePost(String title) {
+		posts.remove(title);
+	}
+
+	public int getCount() {
+		return count;
+	}
 }
