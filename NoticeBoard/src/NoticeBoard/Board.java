d13842e9fa28f2a22eca4b4a829af85f04b59f1a
CHOI-YUN-SEUNG <ryans03@naver.com>
2024-03-29 11:03:48 +0900

    add class Board's processMenu(),inputNumber(),inputString() method

--- NoticeBoard/src/NoticeBoard/Board.java
+++ NoticeBoard/src/NoticeBoard/Board.java
@@ -8,18 +8,42 @@ public class Board {
 	private Scanner scanner;
 	private UserManager userManager;
 	private PostManager postManager;
+	private boolean isRun;
 
 	public Board() {
+		isRun = true;
 		scanner = new Scanner(System.in);
 		userManager = new UserManager();
 		postManager = new PostManager();
 	}
 
 	public void run() {
-		while (true) {
+
+		while (isRun) {
 			printMenu();
-			System.out.print("선택>> ");
+			int choice = inputNumber("선택 ");
+			processMenu(choice);
 		}
+	}
+
+	private int inputNumber(String message) {
+		int number = -1;
+		while (number < 0) {
+			try {
+				System.out.print(message + " : ");
+				String input = scanner.nextLine();
+				number = Integer.parseInt(input);
+			} catch (NumberFormatException e) {
+				System.err.println("숫자를 입력해주시길 바랍니다.");
+			}
+		}
+		return number;
+	}
+
+	private String inputString(String message) {
+		System.out.print(message + ": ");
+		String input = scanner.nextLine();
+		return input;
 	}
 
 	private void printMenu() {
@@ -34,6 +58,61 @@ public class Board {
 		System.out.println("0. 종료");
 		System.out.println("===============");
 	}
-	
-	
+
+	private void processMenu(int choice) {
+		switch (choice) {
+		case 1:
+			registerUser(scanner, userManager);
+			break;
+		case 2:
+			login(scanner, userManager);
+			break;
+		case 3:
+			logout(userManager);
+			break;
+		case 4:
+			registerUser(scanner, userManager);
+			break;
+		case 5:
+			viewBoards(postManager);
+			break;
+		case 6:
+			writePost(scanner, userManager, postManager);
+			break;
+		case 7:
+			myPage(scanner, userManager, postManager);
+			break;
+		case 0:
+			System.out.println("프로그램을 종료합니다.");
+			isRun = false;
+			return;
+		default:
+			System.out.println("올바르지 않은 메뉴 선택입니다. 다시 선택하세요.");
+		}
+	}
+
+	private void myPage(Scanner scanner2, UserManager userManager2, PostManager postManager2) {
+
+	}
+
+	private void writePost(Scanner scanner2, UserManager userManager2, PostManager postManager2) {
+
+	}
+
+	private void viewBoards(PostManager postManager2) {
+
+	}
+
+	private void logout(UserManager userManager2) {
+
+	}
+
+	private void login(Scanner scanner2, UserManager userManager2) {
+
+	}
+
+	private void registerUser(Scanner scanner2, UserManager userManager2) {
+
+	}
+
 }
