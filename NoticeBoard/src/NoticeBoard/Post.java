dc6ef743ede7ee4e7958e1beadd4c1164e5039aa
CHOI-YUN-SEUNG <ryans03@naver.com>
2024-03-29 10:20:37 +0900

    add class Post's getter&setter&constructor

--- NoticeBoard/src/NoticeBoard/Post.java
+++ NoticeBoard/src/NoticeBoard/Post.java
@@ -1,9 +1,19 @@
 package NoticeBoard;
 
 public class Post {
-	String id;
-	String title;
-	String content;
-	
-	
+	private String title;
+    private String content;
+
+    public Post(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
 }
