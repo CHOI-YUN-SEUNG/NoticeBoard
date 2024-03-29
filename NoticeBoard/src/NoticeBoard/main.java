784852698466c13d760364f3eefc928190f9da2a
CHOI-YUN-SEUNG <ryans03@naver.com>
2024-03-29 10:22:06 +0900

    remove class main's unnecessary menus

--- NoticeBoard/src/NoticeBoard/main.java
+++ NoticeBoard/src/NoticeBoard/main.java
@@ -4,14 +4,5 @@ public class main {
 	public static void main(String[] args) {
 		Board board = new Board();
 		board.run();
-		// 콘솔 게시판 만들기
-		// map 활용
-		// 유저 crud
-		// 보드 crud
-		// ㄴ글 작성자만 권한이 있다.
-		// 로그인, 로그아웃, 회원가입, 회원탈퇴,
-		// 회원가입, 사용자 조회, 사용자 글쓰기 권환 수정, 회원 삭제
-		// 글 조회, 글 쓰기, 글 수정, 글 삭제
-
 	}
 }
