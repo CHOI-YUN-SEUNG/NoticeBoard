01fe3687c9383cb520e29c25a98f407931bcb1fe
CHOI-YUN-SEUNG <ryans03@naver.com>
2024-04-01 11:37:38 +0900

    add Override toString

--- NoticeBoard/src/NoticeBoard/AdminNotice.java
+++ NoticeBoard/src/NoticeBoard/AdminNotice.java
@@ -5,4 +5,10 @@ public class AdminNotice extends Post {
 	public AdminNotice(String title, String content, String author) {
 		super(title, content, author);
 	}
+	
+	@Override
+	public String toString() {
+		return " 제목: " + super.getTitle() + " 내용: " + super.getContent() ;
+	}
+	
 }
