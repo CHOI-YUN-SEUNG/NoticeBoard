ef6bc56f8f7fd0e988fec3dc48488e2c0e06cc2f
CHOI-YUN-SEUNG <ryans03@naver.com>
2024-04-01 10:37:36 +0900

    Add properties and methods for managing administrator announcements

--- NoticeBoard/src/NoticeBoard/PostManager.java
+++ NoticeBoard/src/NoticeBoard/PostManager.java
@@ -6,11 +6,14 @@ import java.util.List;
 
 public class PostManager {
 	private HashMap<String, Post> posts;
+	private HashMap<String, UserPost> userPosts;
+	private HashMap<String, AdminNotice> adminNotices;
 
-	private PostManager() {
-		this.posts = new HashMap<>();
+	public PostManager() {
+		userPosts = new HashMap<>();
+		adminNotices = new HashMap<>();
 	}
-
+	
 	private static PostManager instance = new PostManager();
 
 	public static PostManager getInstance() {
@@ -19,7 +22,20 @@ public class PostManager {
 
 	public void addUserPost(String title, String content, String author) {
 		UserPost post = new UserPost(title, content, author);
-		posts.put(title, post);
+		userPosts.put(title, post);
+	}
+
+	public void addAdminPost(String title, String content, String adminId) {
+		AdminNotice post = new AdminNotice(title, content, adminId);
+		adminNotices.put(title, post);
+	}
+
+	public Post getUserPost(String title) {
+		return userPosts.get(title);
+	}
+
+	public Post getAdminPost(String title) {
+		return adminNotices.get(title);
 	}
 
 	public void addAdminNotice(String title, String content, String author) {
@@ -47,6 +63,24 @@ public class PostManager {
 
 	public void removePost(String title) {
 		posts.remove(title);
+	}
+
+	public Post findPost(String author) {
+		List keyset = new ArrayList(posts.keySet());
+		for (Object postTitle : keyset) {
+			Post post = posts.get(postTitle);
+			if (post != null && author.equals(post.getAuthor())) {
+				return post;
+			}
+		}
+		return null;
+	}
+
+	public void removeAllPost(String id) {
+		while (findPost(id) != null) {
+			Post post = findPost(id);
+			removePost(post.getTitle());
+		}
 	}
 
 	public int getSize() {
